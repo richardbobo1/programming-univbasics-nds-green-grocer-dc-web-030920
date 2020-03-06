fcca01d0a7e22bc04ac104ef03e508bcee5fd10b
Richard Bobo <richard.bobo@gmail.com>
2020-03-06 19:07:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -54,7 +54,8 @@ def apply_coupons(cart, coupons)
   while counter < coupon.length 
     item_with_coupon = find_item_by_name_in_collection(coupons[i][:item], cart)
     coupon_item_name = "#{coupons[i][:item]} W/COUPON", 
-  
+    cart_item_with_coupon = find_item_by_name_in_collection[coupon_item_name, cart] 
+    
   
     if item_with_coupon != nil 
        item_with_coupon[:clearance] = true 
